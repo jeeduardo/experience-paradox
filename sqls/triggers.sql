dd88dfafd24a132f11542bbf44fc9c88b8a3a27b
Josephson Eduardo <josephson.eduardo@gmail.com>
2023-03-02 21:42:34 -0500

    Add some trigger

---
+++ sqls/triggers.sql
@@ -0,0 +1,40 @@
+-- UPDATE sales_today TABLE WHEN an order is placed for today...
+DELIMITER $$
+DROP TRIGGER IF EXISTS trg_update_sales_today$$
+CREATE TRIGGER trg_update_sales_today
+AFTER INSERT ON orders FOR EACH ROW 
+BEGIN
+  SET @COUNT =(SELECT COUNT(*) FROM sales_today WHERE date_today = CURRENT_DATE);
+  IF @COUNT = 0 THEN
+    INSERT INTO
+      sales_today (
+        `year`,
+        `month`,
+        `day`,
+        `date_today`,
+        `orders_total`
+      )
+    VALUES
+      (
+        year(current_date),
+        lpad(month(current_date), 2, '0'),
+        lpad(day(current_date), 2, '0'),
+        current_date,
+        NEW.grand_total
+      );
+  ELSE
+    UPDATE
+      sales_today
+    SET
+      sales_today.order_amount_total = sales_today.order_amount_total + NEW.grand_total
+    WHERE
+      sales_today.year = year(current_date)
+      AND sales_today.month = lpad(month(current_date), 2, '0')
+      AND sales_today.day = lpad(day(current_date), 2, '0')
+      AND sales_today.date_today = current_date;
+  END IF;
+END;
+$$
+DELIMITER ;
+
+
