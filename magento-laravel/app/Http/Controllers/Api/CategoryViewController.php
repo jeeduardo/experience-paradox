317b648fd588bb1a48479942ffe9fd70659b1d3d
Josephson Eduardo <josephson.eduardo@gmail.com>
2023-03-06 19:41:10 -0500

    Code comment for CategoryViewController

--- magento-laravel/app/Http/Controllers/Api/CategoryViewController.php
+++ magento-laravel/app/Http/Controllers/Api/CategoryViewController.php
@@ -9,6 +9,11 @@ use Illuminate\Http\Request;
 
 class CategoryViewController extends Controller
 {
+    /**
+     * API controller to get category data and its products
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function __invoke($id)
     {
         $category = Category::findOrFail($id);
